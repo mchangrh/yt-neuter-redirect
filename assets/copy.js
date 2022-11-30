ee36de10bca753e8c104e15f1f815348bb8042a7
Michael C <michael@mchang.name>
2022-11-30 15:31:29 -0500

    add viewport, js formatting

--- assets/copy.js
+++ assets/copy.js
@@ -1,6 +1,6 @@
 function copy(id) {
-    const copyText = document.getElementById(id).value;
-    const button = document.getElementById("copy-" + id);
+    const copyText = document.getElementById(id).value
+    const button = document.getElementById("copy-" + id)
     navigator.clipboard.writeText(copyText)
-    button.innerText = "✅";
+    button.innerText = "✅"
 }
