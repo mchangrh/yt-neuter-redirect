24e2db46c1020f7fbb5c439964621b43ee0ec489
Michael C <michael@mchang.name>
2024-02-06 09:57:30 -0500

    remove /reflow worker

--- assets/copy.js
+++
@@ -1,6 +0,0 @@
-function copy(id) {
-    const copyText = document.getElementById(id).value
-    const button = document.getElementById("copy-" + id)
-    navigator.clipboard.writeText(copyText)
-    button.innerText = "✅"
-}
